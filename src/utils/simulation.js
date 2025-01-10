1dc05860ca6cacc52e908e0d48ff2e5e77530f42
Gabriel <89980949+emptyvoxel@users.noreply.github.com>
2025-01-09 21:16:40 -0300

    Calculate membrane physical attributes

--- src/utils/simulation.js
+++ src/utils/simulation.js
@@ -1,18 +1,39 @@
-export const FARADAY = 9.6487;
-export const RGAS = 8.3143;
-export const KELVIN = 273.15;
+import { Stimulus } from "./classes";
+
+// Physical constants
+const FARADAY = 9.6487;
+const RGAS = 8.3143;
+const KELVIN = 273.15;
 export const IONS = {
     'K': { in: 140, out: 5, charge: 1 },
     'Na': { in: 15, out: 145, charge: 1 },
-}
+};
+
+// Gate kinetics equations (Hodgkin & Huxley, 1952)
+// (Eqns. 12-13)
+const an = v => .01 * (v + 10) / (Math.exp((v + 10) / 10) - 1);
+const bn = v => .125 * Math.exp(v / 80);
+// (Eqns. 20-21)
+const am = v => .1 * (v + 25) / (Math.exp((v + 25) / 10) - 1);
+const bm = v => 4 * Math.exp(v / 18);
+// (Eqns. 23-24)
+const ah = v => .07 * Math.exp(v / 20);
+const bh = v => 1 / (Math.exp((v + 30) / 10) + 1);
+
+// TESTING STUFF
+const N_MODELS = 2;
 
 export class Simulation {
     constructor () {
+        // Simulation type
+        this.type = 'AP';
+        this.voltageClamp = false;
+        this.markovModel = false;
+
         // Membrane potential
         this.mVInit = -60;
         this.mVMax = 60;
         this.mVMin = -90;
-        this.mV = this.mVInit;
 
         // Conductances
         this.gKMax = 36; // Maxium K conductance
@@ -31,6 +52,14 @@ export class Simulation {
         this.mExp = 3;
         this.hExp = 1;
 
+        // NOTE: Will be useful for pharmacology
+        this.hFrac = 1;
+
+        // Gate closing and opening charges
+        this.nOpen = -50; this.nClose = -60;
+        this.mOpen = -35; this.mClose = -60;
+        this.hOpen = -60; this.hClose = -30;
+
         // Ionic equilibrium potentials
         this.EK = -72;
         this.ENa = 55;
@@ -42,6 +71,16 @@ export class Simulation {
         this.celsius = 20;
         this.temperature = this.celsius + KELVIN;
         this.RTzF = RGAS * this.temperature / FARADAY;
+
+        // Stimuli
+        this.stimuli = [
+            new Stimulus(100, 0, .1)
+        ];
+
+        // Plotting resolution
+        this.TIMEBASE = 10;
+        this.POINTS = 500;
+        this.TOTALTIME = this.TIMEBASE;
     }
 
     calcLeak () {
@@ -56,5 +95,111 @@ export class Simulation {
 
     calcNernst (ion) {
         return this.RTzF * Math.log(ion.out / ion.in) / ion.z;
+    }
+
+    run () {
+        this.initialize();
+
+        for (let t = 0; t < this.TOTALTIME; t += this.dt) {
+            if (this.voltageClamp) this.commandV();
+
+            if (this.markovModel) {
+                this.iT = 0;
+
+                for (let k = 1; k < N_MODELS; k++) {
+                    this.markovRates(k);
+                    this.markovGatingCharge();
+                    this.markovState(k);
+                    this.markovCurrent();
+                }
+            } else {
+                this.rates(); // done
+                // this.gatingCharge();
+                this.gates(); // done
+                this.currents(); // done
+            }
+
+            if (this.voltageClamp) {
+                this.gatingCurrents();
+                this.plotVClamp();
+            } else {
+                // this.stimulus();
+                this.voltages(); // done
+            }
+
+            // this.measurements();
+        }
+    }
+
+    initialize () {
+        this.dt = this.TIMEBASE / this.POINTS;
+        this.ddt = this.dt / 1000;
+
+        this.n = 0;
+        this.m = 0;
+        this.h = 0;
+
+        this.calcLeak();
+
+        this.mV = this.mVInit;
+        this.mVold = this.mV;
+    }
+
+    rates () {
+        // TODO: Check for division by zero
+        this.an = an(this.mV - this.nOpen);
+        this.bn = bn(this.mV - this.nClose);
+        this.am = am(this.mV - this.mOpen);
+        this.bm = bm(this.mV - this.mClose);
+        this.ah = ah(this.mV - this.hOpen);
+        this.bh = bh(this.mV - this.hClose);
+
+        if ((this.n + this.m + this.h) === 0) {
+            this.steadyState();
+        }
+    }
+
+    steadyState () {
+        this.n = this.an / (this.an / this.bn);
+        this.m = this.am / (this.am / this.bm);
+        this.h = this.ah / (this.ah / this.bh);
+    }
+
+    gates () {
+        // Derived from Eqns. 7, 15 and 16
+        this.dn = this.an * (1 - this.n) - this.bn * this.n;
+        this.dm = this.am * (1 - this.m) - this.bm * this.m;
+        this.dh = this.ah * (1 - this.h) - this.bh * this.h;
+
+        // Step dt units in time
+        this.n += this.dn * this.dt;
+        this.m += this.dm * this.dt;
+        this.h += this.dh * this.dt;
+
+        // TODO: Check gate
+    }
+
+    currents () {
+        let hNet = this.h * this.hFrac + (1 - this.hFrac);
+
+        // Experimentally obtained probabilities (Fig. 3)
+        this.pK = Math.pow(this.n, this.nExp);
+        this.pNa = Math.pow(this.m, this.mExp) * Math.pow(hNet, this.hExp);
+
+        // Ion conductance (Eqns. 6, 14)
+        this.gK = this.gKMax * this.pK;
+        this.gNa = this.gNaMax * this.pNa;
+
+        // Ionic and leak currents (Eqns. 3-5)
+        this.iNa = this.gNa * (this.mV - this.vNa);
+        this.iK = this.gK * (this.mV - this.vK);
+        this.iL = this.gKLeak * (this.mV - this.vK)
+                + this.gNaLeak * (this.mV - this.vNa);
+        this.iC = -this.Cm * (this.mVold - this.mV) / this.dt;
+        this.iT = this.iT + this.iK + this.iNa;
+    }
+
+    voltages () {
+        this.mV += -this.iT * this.dt;
     }
 }
